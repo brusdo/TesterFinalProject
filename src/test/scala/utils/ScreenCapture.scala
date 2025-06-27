54643264e98c58476268dae17fc58df5a08a0f66
Yanrong Zhang <yanrong.zhang@Mac.broadband>
2025-06-27 12:25:07 +0100

    update takeScreenShot

--- src/test/scala/utils/ScreenCapture.scala
+++ src/test/scala/utils/ScreenCapture.scala
@@ -6,18 +6,18 @@ import org.openqa.selenium.io.FileHandler
 
 import java.text.SimpleDateFormat
 import java.util.Date
+import java.io.File
 
 object ScreenCapture {
 
   def takeScreenshot_Project(driver: WebDriver, fileName: String): Unit = {
 
     val timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date())
-//    val screenShotDir = new File(System.getProperty("user.home") + "/Desktop/ScreenShots")
-//    if(!screenShotDir.exists())
-//      screenShotDir.mkdir()
-//    val srcTimestamp: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
-//    FileHandler.copy(srcTimestamp, new File(s"$screenShotDir/$fileName-$timeStamp.png"))
+    val screenShotDir = new File(System.getProperty("user.home") + "/Desktop/ScreenShots")
+    if (!screenShotDir.exists())
+      screenShotDir.mkdir()
+    val srcTimestamp: File = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.FILE)
+    FileHandler.copy(srcTimestamp, new File(s"$screenShotDir/$fileName-$timeStamp.png"))
 
   }
-
 }

bcef228c9985625607407ad6fe5a9111ba525e71
Bruna de Oliveira <bruna.oliveira@mercator.group>
2025-07-01 11:57:53 +0100

    Checkout feature completed

--- src/test/resources/features/Checkout.feature
+++ src/test/resources/features/Checkout.feature
@@ -1,4 +1,4 @@
-
+@smoke
 Feature: Checkout
 
   Background:
--- src/test/scala/stepdefs/CheckoutSteps.scala
+++ src/test/scala/stepdefs/CheckoutSteps.scala
@@ -8,42 +8,34 @@ import pages.CheckoutPage.{verifyPersonalInformationFields, verifyCheckoutPageTi
 
 
 class CheckoutSteps extends ScalaDsl with EN {
-  Given("""the user adds items to their cart""") { () =>
+  Given("""^the user adds items to their cart$""") { () =>
     addBackpackToCart()
     println("Added Sauce Labs Backpack to cart")
   }
 
-  And("""And the user navigates to the cart page""") { () =>
+  And("""^the user navigates to the cart page$""") { () =>
     navigateToCart()
     println("Navigated to cart page")
   }
 
-  And("""And the user verifies they have 1 or more items in my cart""") { () =>
+  And("""^the user verifies they have 1 or more items in my cart$""") { () =>
     verifyCartHasItems()
     val itemCount = getCartItemsCount()
     println(s"Verified cart has $itemCount item(s)")
   }
 
-  When("""When the user clicks the "Checkout" button on the cart page""") { () =>
+  When("""^the user clicks the "Checkout" button on the cart page$""") { () =>
     clickCheckoutButton()
     println("Clicked Checkout button")
   }
 
-  Then("""Then the user is prompted to enter their personal information""") { () =>
+  Then("""^the user is prompted to enter their personal information$""") { () =>
     verifyPersonalInformationFields()
     println("Verified personal information fields are displayed")
   }
 
-  And("""And the user should be on the checkout page""") { () =>
+  And("""^the user should be on the checkout page$""") { () =>
     verifyCheckoutTitle()
     println("Verified user is on checkout page")
   }
 }
-
-//Scenario: Initiate the checkout process from the cart page
-//  Given the user adds items to my cart
-//  And the user navigates to the cart page
-//  And the user verifies they have 1 or more items in my cart
-//When the user clicks the "Checkout" button on the cart page
-//  Then the user is prompted to enter their personal information
-//  And the user should be on the checkout page

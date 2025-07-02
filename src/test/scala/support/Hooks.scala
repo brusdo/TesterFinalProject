d5374e82300b6d2bf1fb10cce64f365bc4ae8f68
Yanrong Zhang <yanrong.zhang@Mac.broadband>
2025-07-02 09:15:36 +0100

    add extent report

--- src/test/scala/support/Hooks.scala
+++ src/test/scala/support/Hooks.scala
@@ -4,9 +4,9 @@ import io.cucumber.scala.{EN, ScalaDsl, Scenario}
 import org.openqa.selenium.chrome.{ChromeDriver, ChromeOptions}
 import utils.ConfigReader
 import utils.ScreenCapture.takeScreenshot
+import ExtentManager._
 
-
-class Hooks extends  ScalaDsl with EN {
+class Hooks extends ScalaDsl with EN {
 
   val options = new ChromeOptions()
   options.addArguments("--headless=new")
@@ -16,15 +16,27 @@ class Hooks extends  ScalaDsl with EN {
     DriverManager.driver = new ChromeDriver(options)
     DriverManager.driver.manage().window().maximize()
     DriverManager.driver.get(ConfigReader.get("base.url"))
+    scenario: Scenario =>
+      println(s"Starting scenario: ${scenario.getName}")
+      currentTest = Some(extent.createTest(scenario.getName))
   }
 
   After {
     scenario: Scenario =>
-      if(scenario.isFailed){
-        takeScreenshot(DriverManager.driver, "FailedTest")
-      }
-    println("Closing browser after scenario...")
-    DriverManager.driver.quit()
+
+
+      println(s"Finishing scenario: ${scenario.getName}")
+      if (scenario.isFailed) {
+        currentTest.foreach(_.fail("Scenario failed"))
+        val screenshotPath = takeScreenshot(DriverManager.driver, scenario.getName)
+        currentTest.foreach(_.addScreenCaptureFromPath("../" + screenshotPath, "Screenshot"))
+      } else
+        currentTest.foreach(_.pass("Scenario passed"))
+
+      extent.flush()
+
+      println("Closing browser after scenario...")
+      DriverManager.driver.quit()
   }
 
 }
